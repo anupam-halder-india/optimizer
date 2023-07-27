a312ed7f7af34d9245eca4dd15832dab49284be5
anupam-halder <102725001+anupam-halder-india@users.noreply.github.com>
2023-07-27 12:06:14 +0000

    mantanince commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# optimizer
+# 🚀 Optimizer 🚀
+
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+[![GitHub Issues](https://img.shields.io/github/issues/your-username/optimizer)](https://github.com/your-username/optimizer/issues)
+[![GitHub Stars](https://img.shields.io/github/stars/your-username/optimizer)](https://github.com/your-username/optimizer/stargazers)
+
+> A powerful and efficient tool to optimize your life!
+
+## Overview 📝
+
+(in progress)
+
+## Features 🌟
+
+- 📅 **Time Management**: Efficiently plan your day, week, or month with ease.
+- 📝 **Task Organizer**: Keep track of all your tasks and to-dos effortlessly.
+- 🚦 **Productivity Boost**: Find ways to increase productivity and stay focused.
+- 🧠 **Decision-making Support**: Get smart suggestions for better decision-making.
+- 📊 **Data Analytics**: Analyze your progress and trends to enhance performance.
+
+## Installation 🛠️
+
+Follow these simple steps to get Optimizer up and running on your local machine:
+
+```bash
+(in progress)
+```
+
+## License 📜
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Contact 📧
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
+#include <string>
+#include <algorithm> // For std::find
+using std::cout;
+using std::endl;
+using std::cin;
+using std::string;
+
+// Function prototypes
+bool Switch(const string& str);
+
 int main() {
-  std::cout << "Hello World!" << std::endl;
+  // Asking for permissions
+
+  // Installation permission of GitLab Runner
+  string ans;
+  cout << "Do you want to install GitLab Runner? (yes/no)" << endl;
+  cin >> ans;
+
+  // Validation of the chosen option
+  while (!Switch(ans)) {
+    cout << "Do you want to install GitLab Runner? (yes/no)" << endl;
+    cin >> ans;
+  }
+
+  // Execution of the instructions according to the given permissions
+
   return 0;
+}
+
+// Function definition
+
+// Choice validator
+bool Switch(const string& str) {
+  string yes[] = { "yes", "Yes", "YES", "y", "Y" };
+  string no[] = { "no", "No", "NO", "n", "N" };
+
+  // Check if the input matches any of the elements in the arrays
+  if (std::find(std::begin(yes), std::end(yes), str) != std::end(yes)) {
+    return true; // Return true for a valid input
+  } else if (std::find(std::begin(no), std::end(no), str) != std::end(no)) {
+    return true; // Return true for a valid input
+  } else {
+    cout << "Please choose from 'yes' or 'no'." << endl;
+    return false; // Return false for an invalid input
+  }
 }
