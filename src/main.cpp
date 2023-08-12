748e86a84770389746186831ac05ddfe55a2bd5e
anupam-halder <102725001+anupam-halder-india@users.noreply.github.com>
2023-08-12 11:24:19 +0000

    headder file

--- src/main.cpp
+++ src/main.cpp
@@ -29,44 +29,8 @@ SOFTWARE.
 #include <algorithm>  // For std::find
 #include <curl/curl.h>  // for curl
 #include <fstream>  // for the os detction
-#include <unistd.h>  // for exec()
-
-using std::cout;
-using std::endl;
-using std::cin;
-using std::string;
-using std::find;
-using std::begin;
-using std::end;
-using std::cerr;
-using std::ifstream;
-using std::ios;
-
-// The global necessary variables
-
-// necessary booleans
-const string yes[] = { "yes", "Yes", "YES", "y", "Y" };
-const string no[] = { "no", "No", "NO", "n", "N" };
-
-// colours
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define YELLOW "\033[33m"
-#define BLUE "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN "\033[36m"
-#define BOLD "\033[1m"
-#define RESET "\033[0;0m"
-
-// Function prototypes
-
-// choice validator
-int Switch(const string& str);
-// curl
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, string* response);
-// os dector
-int osDection();
-const int osType = osDection();
+#include "include/imports.hpp"
+#include "include/functions.hpp"
 
 int main() {
   // initilizing curl
@@ -78,28 +42,6 @@ int main() {
 
   // all the variables used
 
-  // necessary for curl
-  string response;
-
-  // the name section
-  string name;
-
-  // gitlab runner section
-  string arch;
-  string GRIP;  // Gitlab Runner Installation Permission
-  int wrapper;
-
-  // from arch secrtion of gitlab runner
-  string arm32;
-  string arm64;
-  string amd64;
-
-  // gitlab runner installation command exicution section
-  string url;
-  string dpkging;
-  string retryal = "yes";
-  string retryal2 = "yes";
-
   // Asking for permissions
 
   // name for coustomization pourposes
@@ -107,11 +49,9 @@ int main() {
     cout << BOLD << MAGENTA << "[1]" << RESET << " Please enter your name, we need it for customization purposes (2 to 25 characters, no spaces): ";
     getline(cin, name);
 
-    // to check if there is space or not
-    bool hasSpace = (name.find(' ') != string::npos);
     // Check the length of the name
     int nameLength = name.length();
-    if (nameLength >= 2 && nameLength <= 25 && !hasSpace) {
+    if (nameLength >= 2 && nameLength <= 25 && !hasSpaces) {
       break;  // Exit the loop if the name is valid
     } else {
       cout << RED << "[ERROR]" << RESET << "Invalid name length. Please try again." << endl;
@@ -129,17 +69,6 @@ int main() {
 	if (osType == 3) { string armWord = "arm"; } else string armWord = "";
         cout << BOLD << MAGENTA << "[2.1] " << RESET << " What is your architecture, you can say 'cancel', to cancel the installation of gitlab runner ((arm32/armhf), arm64, amd64, aarch64, i386, ppc64el, s390x " + armWord + "): ";
         getline(cin, arch);
-	bool hasSpaces = (arch.find(' ') != string::npos);
-	string arm32[] = {"ARM32", "Arm32", "arm32", "armhf", "ARMHF", "Armhf"};
-	string arm64[] = {"ARM64", "Arm64", "arm64"};
-	string amd64[] = {"AMD64", "Amd64", "amd64"};
-	string aarch64[] = {"AARCH64", "Arrch64", "aarch64"};
-	string i386[] = {"I386", "i386"};
-	string ppc64el[] = {"PPC64EL", "Ppc64el", "ppc64el"};
-	string s390x[] = {"S390x", "S390X", "s390x"};
-	string arm[] = {"ARM", "Arm", "arm"};
-	string cancel[] = {"CANCEL", "Cancle", "cancle"};
-
         if (find(begin(arm32), end(arm32), arch) != end(arm32)) { string arch = arm32[3]; break; }
 	else if (find(begin(arm64), end(arm64), arch) != end(arm64)) { string arch = arm64[2]; break; }
 	else if (find(begin(i386), end(i386), arch) != end(i386)) { string arch = i386[1]; break; }
@@ -157,22 +86,19 @@ int main() {
   }
   
   // Execution of the instructions according to the given permissions
-  if (wrapper == 1) { 
-    const char* dpkgCommand;
-    const char* dpkgArgs[4];
+  if (wrapper == 1) {
     while (true) {
       // gitlab runner installation url
       while (retryal == "yes") {
-        if (osType == 1 || osType == 2) { string url = "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_" + arch + ".deb"; string dpkging = "gitlab-runner_" + arch + ".deb"; dpkgCommand = "/usr/bin/dpkg"; dpkgArgs[0] = dpkgCommand; dpkgArgs[1] = "-Uvh"; dpkgArgs[2] = dpkging.c_str(); dpkgArgs[3] = nullptr; }
-	else if (osType == 3 || osType == 4) { string url = "https://gitlab-runner-downloads.s3.amazonaws.com/latest/rpm/gitlab-runner_" + arch + ".rpm"; string dpkging = "rpm -i gitlab-runner_" + arch + ".rpm && rpm -Uvh gitlab-runner_" + arch + ".rpm"; dpkgCommand = "/bin/rpm"; dpkgArgs[0] = dpkgCommand; dpkgArgs[1] = "-Uvh"; dpkgArgs[2] = dpkging.c_str(); dpkgArgs[3] = nullptr; }
+        if (osType == 1 || osType == 2) { string url = "https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_" + arch + ".deb"; string dpkging = "dpkg -i gitlab-runner_" + arch + ".deb"; }
+	else if (osType == 3 || osType == 4) { string url = "https://gitlab-runner-downloads.s3.amazonaws.com/latest/rpm/gitlab-runner_" + arch + ".rpm"; string dpkging = "rpm -i gitlab-runner_" + arch + ".rpm && rpm -Uvh gitlab-runner_" + arch + ".rpm"; }
 	else if (osType == 404) { cout << BOLD << RED << "[ERROR]" << RESET << "error 404, you are using unknown oprating system to install git lab runner, can't install" << endl; break; }
 	else { cout << BOLD << RED << "[ERROR]" << RESET << "unknown error cant install gitlab runner" << endl; break;}
-      }
         // gitlab runner installation through its url and curl lib
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
       
         // checking if it is installed or not
-        if (res != CURLE_OK) {
+        if (res != CURLE_OK) { 
 	  cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
 	  cout << BOLD << CYAN << "[i] " << RESET << "do you want to retry? (yes/no): ";
 	  getline(cin, retryal);
@@ -183,23 +109,25 @@ int main() {
 	    else { cout << BOLD << RED << "[ERROR] " << RESET << "pls choose from yes or no" << endl; }
 	  }
 	}
+        else { cout << BOLD << GREEN << "[!] " << RESET << "Download completed successfully." << endl; } }
+	curl_easy_cleanup(curl);
+	break;
+      
+      while (retryal2 == "yes") { 
+        int Dpackaging = system(dpkging.c_str());
+
+        if (Dpackaging == 0) { cout << BOLD << GREEN "[!] " << RESET << "Installation completed successfully." << endl; break; }
         else { 
-	  cout << BOLD << GREEN << "[!] " << RESET << "Download completed successfully." << endl;
-	  cout << BOLD << CYAN << "[i] " << RESET << "Doing installation..." << endl;
-	}
-      curl_easy_cleanup(curl);
-      while (retryal2 == "yes") {
-        if (execv(dpkgCommand, (char* const*)dpkgArgs) == -1) {
+	  cerr << BOLD << RED "[ERROR] " << RESET << "Installation failed with error code: " << Dpackaging << endl;
+	  cout << BOLD << CYAN << "[i] " << RESET << "do you want to retry (yes/no): ";
+	  getline(cin, retryal2);
+	  Switch(retryal2);
 	  while (true) {
-	    cerr << BOLD << RED << "[ERROR] " << RESET << "Failed to execute dpkg command." << endl;
-	    cout << BOLD << CYAN << "[?] " << RESET << "Do you want to retry (yes/no): ";
-	    getline(cin, retryal2);
 	    if (find(begin(yes), end(yes), retryal2) != end(yes)) {string retryal2 = "yes"; break;}
 	    else if (find(begin(no), end(no), retryal2) != end(no)) { string retryal2 = "no"; break; }
 	    else { cout << BOLD << RED << "[ERROR] " << RESET << "pls choose from yes or no" << endl; }
 	  }
 	}
-	else { cout << BOLD << GREEN << "[!] " << RESET << "Download installation successfully." << endl; break; }
       }
       break;
     }
@@ -210,62 +138,3 @@ int main() {
 // }
   return 0;
 }
-
-// Function definitions
-// the curl function
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, string* response) {
-  size_t totalSize = size * nmemb;
-  response->append((char*)contents, totalSize);
-  return totalSize;
-}
-
-// Choice validator
-int Switch(const string& str) {
-  // Check if the input matches any of the elements in the global 'yes' or 'no'
-  bool hasSpaces = (str.find(' ') != string::npos);
-  if (find(begin(yes), end(yes), str) != end(yes)) {
-    return 1;  // Return true for a valid input
-  } else if (find(begin(no), end(no), str) != end(no)) {
-    return 2;  // Return true for a valid input
-  } else if (hasSpaces || str.empty()) {
-    cout << BOLD << RED << "[ERROR] " << RESET << "pls don't enter spaces or emptyness" << endl;
-    return 3;
-  } else {
-    cout << BOLD << RED << "[ERROR]" << RESET << "Please choose from 'yes' or 'no'." << endl;
-    return 4;  // Return false for an invalid input
-  }
-}
-
-// os dector
-int osDection() {
-  ifstream releaseFile("/etc/os-release");
-  if (!releaseFile.is_open()) {
-    cerr << BOLD << RED << "[ERROR]" << RESET << " opening /etc/os-release" << endl;
-    return 404;  // Return 404 if the file cannot be opened
-  }
-
-  string line;
-  string distribution;
-
-  while (getline(releaseFile, line)) {
-    if (line.find("ID=") != string::npos) {
-      distribution = line.substr(3);  // Extract the value after "ID="
-      break;
-    }
-  }
-
-  transform(distribution.begin(), distribution.end(), distribution.begin(), ::tolower);
-
-  if (distribution == "ubuntu") {
-    return 1;  // Return 1 for Ubuntu
-  } else if (distribution == "debian") {
-    return 2;  // Return 2 for Debian
-  } else if (distribution == "centos") {
-    return 4;  // Return 4 for CentOS
-  } else if (distribution == "rhel" || distribution == "redhat") {
-    return 3;  // Return 3 for Red Hat
-  } else {
-    return 404;  // Return 404 for other distributions
-  }
-}
-
