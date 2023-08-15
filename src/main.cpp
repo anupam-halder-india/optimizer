6f90c405fa51d3f7dca701f118d6601e441822aa
Anupam Halder <102725001+anupam-halder-india@users.noreply.github.com>
2023-08-15 13:22:09 +0530

    Update main.cpp
    Triggering the github actions build process
    
    Signed-off-by: Anupam Halder <102725001+anupam-halder-india@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ SOFTWARE.
 #include "include/modules/StrDect.inl"
 
 int main() {
-  // initilizing curl
+  // Initilizing curl
   CURL* curl = curl_easy_init();
   if (!curl) { cerr << "Failed to initialize libcurl" << endl; return 1; } 
 
