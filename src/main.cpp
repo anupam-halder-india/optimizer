a3af4c81d2a5d19564fea6d5ff9692b7fac8abfb
anupam-halder <102725001+anupam-halder-india@users.noreply.github.com>
2023-08-26 14:03:24 +0000

    updated action yml

--- src/main.cpp
+++ src/main.cpp
@@ -68,7 +68,7 @@ int main() {
     }
   }
   
-  // Asking for installation permission of gitLab runner
+  // Asking for installation permission for gitLab runner
   string GRIP;
   string arch;
   int wrapper = 1;
